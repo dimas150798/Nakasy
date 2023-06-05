9ace2603ec5004b0d344a868c184eecc8c065dd5
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-06-05 11:19:37 +0700

    Menambah Fitur Terminasi Pada Sidebar

--- application/views/template/sidebarAdmin.php
+++ application/views/template/sidebarAdmin.php
@@ -37,12 +37,12 @@
                         <!-- Customer Menu -->
                         <div class="sb-sidenav-menu-heading">Customer</div>
 
-                        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#dataCustomer" aria-expanded="false" aria-controls="dataCustomer">
                             <div class="sb-nav-link-icon"><img src="<?php echo base_url(); ?>vendor/bootstrap-icons/icons/people-fill.svg" alt="Bootstrap" ...></div>
                             Customer
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <div class="collapse" id="dataCustomer" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
                                 <a class="nav-link" href="<?php echo base_url('admin/DataPelanggan/C_DataPelanggan') ?>">
                                     <div class="sb-nav-link-icon"><img src="<?php echo base_url(); ?>vendor/bootstrap-icons/icons/people-fill.svg" alt="Bootstrap" ...></div>
