24694df5145b2f4256d7e91032b9c51570e18772
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-06-08 11:11:56 +0700

    Memperbaiki jarak pada form

--- application/views/admin/DataSales/V_TambahSales.php
+++ application/views/admin/DataSales/V_TambahSales.php
@@ -24,22 +24,22 @@
 
                         <form method="POST" action="<?php echo base_url('admin/DataSales/C_TambahSales/TambahSalesSave') ?>">
 
-                            <div class="row mt-3">
-                                <div class="col-sm-4">
+                            <div class="row">
+                                <div class="col-sm-4 mt-3">
                                     <label for="nama_sales" class="form-label" style="font-weight: bold;"> Nama : <span class="text-danger">*</span></label>
                                     <input type="text" class="form-control" name="nama_sales" value="" placeholder="Masukkan nama...">
                                     <div class="bg-danger">
                                         <small class="text-white"><?php echo form_error('nama_sales'); ?></small>
                                     </div>
                                 </div>
-                                <div class="col-sm-4">
+                                <div class="col-sm-4 mt-3">
                                     <label for="phone_sales" class="form-label" style="font-weight: bold;"> Telephone : <span class="text-danger">*</span></label>
                                     <input type="number" class="form-control" name="phone_sales" value="" placeholder="Masukkan telephone...">
                                     <div class="bg-danger">
                                         <small class="text-white"><?php echo form_error('phone_sales'); ?></small>
                                     </div>
                                 </div>
-                                <div class="col-sm-4">
+                                <div class="col-sm-4 mt-3">
                                     <label for="id_jabatan" class="form-label" style="font-weight: bold;"> Nama Jabatan : <span class="text-danger">*</span></label>
                                     <select id="id_jabatan" name="id_jabatan" class="form-control" required>
                                         <option value="">Pilih Jabatan :</option>
@@ -54,7 +54,6 @@
                                     </div>
                                 </div>
                             </div>
-
 
                             <div class="row mt-3">
                                 <div class="col-sm-12 d-flex justify-content-end">
