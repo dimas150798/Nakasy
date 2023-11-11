221ed2811369fe2756bf8392e88500e9cb61ed33
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-11-11 07:17:16 +0700

    Status

--- application/controllers/C_FormLogin.php
+++ application/controllers/C_FormLogin.php
@@ -146,6 +146,22 @@ class C_FormLogin extends CI_Controller
         $this->MikrotikKraksaanModel->Terminasi_Kraksaan_DESC($bulan, $tahun, $TanggalAkhir);
     }
 
+
+    public function Terminasi_KraksaanPaiton_DESC()
+    {
+        date_default_timezone_set("Asia/Jakarta");
+        $bulan = date("m");
+        $tahun = date("Y");
+
+        // Menampilkan tanggal pada akhir bulan
+        $tanggal_akhir = cal_days_in_month(CAL_GREGORIAN, $bulan, $tahun);
+
+        // Menggabungkan tanggal, bulan, tahun
+        $TanggalAkhir = $tahun . '-' . $bulan . '-' . $tanggal_akhir;
+
+        $this->MikrotikPaitonModel->Terminasi_KraksaanPaiton_DESC($bulan, $tahun, $TanggalAkhir);
+    }
+
     public function enableAuto()
     {
         $this->MikrotikModel->EnableAuto();
