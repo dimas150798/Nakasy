c7f61158cac23513dc77179f6727c6ef738da09e
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-12-11 09:29:00 +0700

    Memperbarui fitur data sudah lunas agar membaca lebih cepat

--- application/models/M_BelumLunas.php
+++ application/models/M_BelumLunas.php
@@ -11,9 +11,8 @@ class M_BelumLunas extends CI_Model
         data_customer.name_pppoe, data_customer.password_pppoe, data_customer.id_pppoe, data_customer.alamat_customer, data_customer.email_customer, 
         DAY(data_customer.start_date) as tanggal, data_customer.stop_date, data_customer.nama_area, data_customer.deskripsi_customer, data_customer.nama_sales, data_customer.disabled,
         data_pembayaran.order_id, data_pembayaran.gross_amount, data_pembayaran.biaya_admin, 
-        data_pembayaran.nama_admin, data_pembayaran.keterangan, data_pembayaran.payment_type, data_pembayaran.transaction_time, data_pembayaran.expired_date,
-        data_pembayaran.bank, data_pembayaran.va_number, data_pembayaran.permata_va_number, data_pembayaran.payment_code, data_pembayaran.bill_key, 
-        data_pembayaran.biller_code, data_pembayaran.pdf_url, data_pembayaran.status_code, data_paket.nama_paket as namaPaket, data_paket.harga_paket
+        data_pembayaran.nama_admin, data_pembayaran.keterangan, data_pembayaran.transaction_time, data_pembayaran.expired_date,
+        data_paket.nama_paket as namaPaket, data_paket.harga_paket
 
         FROM data_customer
         LEFT JOIN data_paket ON data_customer.nama_paket = data_paket.nama_paket
@@ -33,13 +32,7 @@ class M_BelumLunas extends CI_Model
     public function JumlahBelumLunas($bulan, $tahun, $tanggalAkhir)
     {
         $query   = $this->db->query("SELECT 
-        data_customer.id_customer, data_customer.kode_customer, data_customer.phone_customer, data_customer.nama_customer, data_customer.nama_paket, 
-        data_customer.name_pppoe, data_customer.password_pppoe, data_customer.id_pppoe, data_customer.alamat_customer, data_customer.email_customer, 
-        data_customer.start_date, data_customer.stop_date, data_customer.nama_area, data_customer.deskripsi_customer, data_customer.nama_sales,
-        data_pembayaran.order_id, data_pembayaran.gross_amount, data_pembayaran.biaya_admin, 
-        data_pembayaran.nama_admin, data_pembayaran.keterangan, data_pembayaran.payment_type, data_pembayaran.transaction_time, data_pembayaran.expired_date,
-        data_pembayaran.bank, data_pembayaran.va_number, data_pembayaran.permata_va_number, data_pembayaran.payment_code, data_pembayaran.bill_key, 
-        data_pembayaran.biller_code, data_pembayaran.pdf_url, data_pembayaran.status_code, data_paket.nama_paket as namaPaket, data_paket.harga_paket
+        data_customer.id_customer
 
         FROM data_customer
         LEFT JOIN data_paket ON data_customer.nama_paket = data_paket.nama_paket
